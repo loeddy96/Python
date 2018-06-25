01beb0366a516f51915ff4fce699cdf07e19b9f4
loeddy96 <40173702+loeddy96@users.noreply.github.com>
2018-06-25 14:19:01 +0800

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,12 @@
+n1=input("enter a number")
+n2=input("enter a number")
+n3=input("enter a number")
+n4=input("enter a number")
+n5=input("enter a number")
+n1=int(n1)
+n2=int(n2)
+n3=int(n3)
+n4=int(n4)
+n5=int(n5)
+result=n1+n2+n3+n4+n5
+print(result)
